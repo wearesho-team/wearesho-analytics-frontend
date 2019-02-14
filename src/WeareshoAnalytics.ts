60688ae68d7432e6c6e8a75196c4ef1e1c0ab89c
Beton <aleksa.yagich@gmail.com>
2019-02-15 01:56:44 +0200

    Implement building

--- src/WeareshoAnalytics.ts
+++ src/WeareshoAnalytics.ts
@@ -1,6 +1,28 @@
 import { AxiosInstance, AxiosResponse } from "axios";
 
-import { Response, Request, FingerPrintGenerator, FingerPrintComponents } from "./interfaces";
+export namespace Response {
+    export type AplicationInfo = AxiosResponse<{
+        readonly name: string;
+        readonly version: string;
+    }>;
+}
+
+export namespace Request {
+    export interface RegisterFingerPrint {
+        [key: string]: string;
+    }
+}
+
+export interface FingerPrintComponents {
+    resolution: string;
+    timezone_offset: number;
+    [K: string]: any;
+}
+
+export type FingerPrintGenerator = () => Promise<{
+    token: string;
+    components: FingerPrintComponents;
+}>;
 
 export class WeareshoAnalytics {
     protected generateFingerPrint: FingerPrintGenerator;
